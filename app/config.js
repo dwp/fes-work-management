97a45dad40edfa4e93954dbcf0d08e975354c565
Paul Michael Smith <github@airfront.co.uk>
2017-01-04 09:04:34 +0000

    lists archived versions (#3)

--- app/config.js
+++ app/config.js
@@ -3,8 +3,9 @@
 // Note: prototype config can be overridden using environment variables (eg on heroku)
 
 module.exports = {
+  
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Fraud & Error Service',
 
   // Default port that prototype runs on
   port: '3000',
@@ -21,6 +22,7 @@ module.exports = {
   // Cookie warning - update link to service's cookie page.
   cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>',
   
+  // use the routing for sub applications within app
   useSubapplications: true
 
 }
